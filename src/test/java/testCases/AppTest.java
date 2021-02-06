5b02cedb4ade4f02faaea30bdbf1d0dec149388a
purushpurush225 <purushpurush225@gmail.com>
2021-02-06 16:34:28 +0530

    intial commits

---
+++ src/test/java/testCases/AppTest.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.Out.Println("hi");
+    }
+}
