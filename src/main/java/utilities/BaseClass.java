f8d4a88a07979e1294ececb7790f5057092485c8
purushpurush225 <purushpurush225@gmail.com>
2021-02-08 11:29:06 +0530

    first test scipt

---
+++ src/main/java/utilities/BaseClass.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public WebDriver driver;
+
+	public WebDriver intilisebrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+
+	}
+
+}
--- src/test/java/testCases/homePageTest.java
+++ src/test/java/testCases/homePageTest.java
@@ -1,12 +1,61 @@
 package testCases;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Reporter;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+
 import org.testng.annotations.Test;
 
+import pageObjectModel.HomePageObjects;
+import utilities.BaseClass;
+
 public class homePageTest {
+	public WebDriver driver;
+	public BaseClass base;
+	public HomePageObjects hm;
 	
-    @Test
-    public void shouldAnswerWithTrue()
+    @BeforeMethod
+    public void Initializebrowser()
     {
-       System.out.println("hi");
+    	Reporter.log("=====Browser Session Started=====", true);
+      base = new BaseClass();
+      driver= base.intilisebrowser();
+     
     }
+    @Test
+    public void homePage() throws InterruptedException
+    {
+    	 driver.get("https://www.goibibo.com/");
+    	hm = new HomePageObjects(driver);
+    	Actions a = new Actions(driver);
+    	a.moveToElement(hm.from()).click().sendKeys("Tirupati").perform();
+    	List<WebElement> values = driver.findElements(By.xpath("//div/ul/li"));
+    	Thread.sleep(2000);
+    	for(WebElement value : values)
+    	{
+    		String v = value.getText();
+    		System.out.println(v);
+    		
+    		
+    	}
+    	
+  
+    }
+    
+    
+    
+	/*
+	 * @AfterSuite public void closeApplication() { driver.close();
+	 * Reporter.log("=====Browser Session End=====", true); }
+	 */
+    
+    
 }
